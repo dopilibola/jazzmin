428bb227c5ba338a5ed7a2c5d889f921fded9e84
Your Name <your.email@example.com>
2025-01-10 14:35:19 +0500

    2

--- project/bassic/maskan/urls.py
+++ project/bassic/maskan/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .import views
+# from ..one_to_one import views, url
+
 urlpatterns = [
     path('', views.home, name='home' ),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout' ),
     path('register/', views.register_user, name='register'),
     path('update_password/', views.update_password, name='update_password' ),
+    path('/one-to-one/', include('one_to_one.urls'))
 ]
--- project/bassic/one_to_one/urls.py
+++ project/bassic/one_to_one/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_infodata, name='add_infodata'),
+    path('info_data/', views.add_infodata, name='add_infodata'),
     path('success/', views.contact_success, name='contact_success'),
 ]
