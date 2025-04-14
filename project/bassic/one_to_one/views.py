212252a2e34196a5d819565bf65de6ba390afed4
Your Name <your.email@example.com>
2025-04-14 20:14:19 +0500

    telegram bot

--- project/bassic/one_to_one/views.py
+++ project/bassic/one_to_one/views.py
@@ -2,28 +2,65 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import InfodataForm, QabristonForm, ImageForm
 from .models import Image
+import requests
+
+BOT_TOKEN = '7655485724:AAGERQEXt2IEIcvLU9iSh39ILIR1ONu-0aM'
+CHAT_ID = '7150214500'
 
 # Create your views here
+# @login_required(login_url='/login/')
+# def add_infodata(request):
+#     if not request.user.is_authenticated:
+#         return render(request, 'login.html')
+    
+#     else:
+
+#         if request.method == 'POST':
+#             form = InfodataForm(request.POST)
+#             if form.is_valid():
+#                 infodata = form.save(commit=False)
+#                 infodata.created_by = request.user
+#                 infodata.save()
+#                 form.save()
+#                 return redirect('contact_success') 
+#         else:
+#             form = InfodataForm()
+#         return render(request, 'infodata.html', {'form':form})
+
+
+
+
+
+def send_telegram_message(message):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    data = {
+        'chat_id': CHAT_ID,
+        'text': message
+    }
+    requests.post(url, data=data)
+
 @login_required(login_url='/login/')
 def add_infodata(request):
     if not request.user.is_authenticated:
         return render(request, 'login.html')
-    
     else:
-
         if request.method == 'POST':
             form = InfodataForm(request.POST)
             if form.is_valid():
                 infodata = form.save(commit=False)
                 infodata.created_by = request.user
                 infodata.save()
-                form.save()
+
+                # 🔔 Telegramga xabar yuborish
+                message = f"Yangi infodata qo‘shildi!\nUser: {request.user.username}\nID: {infodata.id}"
+                send_telegram_message(message)
+
                 return redirect('contact_success') 
         else:
             form = InfodataForm()
-        return render(request, 'infodata.html', {'form':form})
-
+        return render(request, 'infodata.html', {'form': form})
 # views.py
+
 
 def qabr(request):
     if request.method == 'POST':  
@@ -42,6 +79,8 @@ def contact_success(request):
     return render(request, 'success.html')
 
 
+
+
 def upload_image(request):
     if request.method == 'POST' and request.FILES['image']:
         form = ImageForm(request.POST, request.FILES)
@@ -51,6 +90,9 @@ def upload_image(request):
     else:
         form = ImageForm()
     return render(request, 'upload_image.html', {'form': form})
+
+
+
 
 # Rasm ro'yxatini ko'rsatish
 def image_list(request):
