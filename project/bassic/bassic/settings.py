ab3c268479ec04678a4e931162cbd98fec709536
Your Name <your.email@example.com>
2025-04-26 20:06:26 +0500

    setting

--- project/bassic/bassic/settings.py
+++ project/bassic/bassic/settings.py
@@ -9,8 +9,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # SECRET_KEY = os.getenv('SECRET_KEY')
